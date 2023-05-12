c4556a92009e1ab7d3651415ffe2c94f5ffb5426
anilatici <anillatici@gmail.com>
2023-05-12 01:54:03 +0100

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,10 @@ Route::get('/blog', [\App\Http\Controllers\RecipeController::class, 'index']);
 
 Route::get('/blog/{recipe}', [\App\Http\Controllers\RecipeController::class, 'show']);
 
+Route::get('/blog/create/post', [\App\Http\Controllers\RecipeController::class, 'create']); 
+Route::post('/blog/create/post', [\App\Http\Controllers\RecipeController::class, 'store']); 
+Route::get('/blog/{recipe}/edit', [\App\Http\Controllers\RecipeController::class, 'edit']); 
+Route::put('/blog/{recipe}/edit', [\App\Http\Controllers\RecipeController::class, 'update']); 
+Route::delete('/blog/{recipe}', [\App\Http\Controllers\RecipeController::class, 'destroy']); 
+
 
