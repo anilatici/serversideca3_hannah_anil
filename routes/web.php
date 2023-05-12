c3c5544132072e77753135d92ec907d8f6411292
anilatici <anillatici@gmail.com>
2023-05-12 01:49:49 +0100

    added index and show routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
-use App\Http\Controllers\PostsController;
-use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,32 +13,14 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
-
-Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
-
-Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
-
-Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
-
-Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
-
-Route::put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
-
-Route::delete('/posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
 
 
-Route::get('/', [PagesController::class, 'index']);
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::resource('/blog', PostsController::class);
+Route::get('/blog', [\App\Http\Controllers\RecipeController::class, 'index']);
 
-Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::get('/', 'HomeController@index');
-Route::get('/recipes', 'RecipesController@index');
-Route::get('/about', 'AboutController@index');
-Route::get('/contact', 'ContactController@index');
+Route::get('/blog/{recipe}', [\App\Http\Controllers\RecipeController::class, 'show']);
 
 
