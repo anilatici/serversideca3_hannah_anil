e85ccb1571e693f887b8bd571a97251e9f1d544e
anilatici <anillatici@gmail.com>
2023-05-13 05:17:44 +0100

    changed index to show recipe images

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,26 +2,25 @@
 
 @section('content')
     <div class="container mx-auto px-4">
-        <div class="flex flex-wrap -mx-2">
-            <div class="w-full px-2 py-4">
-                 <div class="flex flex-wrap -mx-2">
-                    <div class="w-2/3 px-2">
-                        <h1 class="index-heading text-4xl font-bold mb-2">Food!</h1>
-                        <p class="text-base mb-4">Enjoy reading our recipes. Click on a recipe to read!</p>
-                    </div>
-                    <div class="w-1/3 px-2 flex flex-col items-start">
-                        <p class="text-base mb-2">Create new Recipe</p>
-                        <a href="/blog/create/post" class="index-button py-2 px-4 bg-blue-500 text-white text-sm rounded">Add Recipe</a>
-                    </div>
-                </div>                
-                @forelse($recipes as $recipe)
-                    <ul class="list-none pl-0 mb-2">
+        <div class="px-2 py-4">
+            <h1 class="index-heading text-4xl font-bold mb-2">Food Blog</h1>
+            <p class="text-base mb-4">Enjoy reading our recipes. Click on a recipe to read!</p>
+            <p class="text-base mb-2">Create new Recipe</p>
+            <a href="/blog/create/post" class="index-button py-2 px-4 bg-blue-500 text-white text-sm rounded mb-4">Add Recipe</a>
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            @forelse($recipes as $recipe)
+                <div class="px-2">
+                    <a href="./blog/{{ $recipe->id }}">
+                        <img class="w-full" src="{{ $recipe->recipe_img }}" alt="{{ $recipe->recipe_name }}" class="object-cover w-full h-64 rounded">
+                    </a>
+                    <ul class="list-none pl-0 mt-2">
                         <li class="py-1"><a href="./blog/{{ $recipe->id }}" class="index-link text-blue-500 hover:underline">{{ ucfirst($recipe->recipe_name) }}</a></li>
                     </ul>
-                @empty
-                    <p class="index-warning text-yellow-500">No recipes available</p>
-                @endforelse
-            </div>
+                </div>
+            @empty
+                <p class="index-warning text-yellow-500">No recipes available</p>
+            @endforelse
         </div>
     </div>
 @endsection
