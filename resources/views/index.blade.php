a5b2b7cd34f5aa19beae74b537b2c28e9559c872
anilatici <anillatici@gmail.com>
2023-05-13 05:46:01 +0100

    edited the text background

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,9 @@
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    <span style="background-color: dimgray;">Welcome to Delicious Recipes!</span>
+                <h1 class="sm:text-white text-4xl uppercase font-bold text-shadow-md pb-14">
+                    <span style="background-color: dimgray; display: inline-block; padding: 10px 20px; border-radius: 8px; font-size: 3.5rem;">Welcome to Delicious Recipes!</span>
                 </h1>
-                
                 <a 
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
