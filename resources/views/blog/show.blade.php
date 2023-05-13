a9deaa41b35aa707e6d806e53681da0d09e44620
anilatici <anillatici@gmail.com>
2023-05-13 04:08:44 +0100

    installed laravel mix and added tailwind css show

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
+
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12 pt-2">
-                <a href="/blog" class="btn btn-outline-primary btn-sm">Go back</a>
-                <h1 class="display-one">{{ ucfirst($recipe->recipe_name) }}</h1>
-                <p>{!! $recipe->recipe_description !!}</p>
+    <div class="container mx-auto px-4">
+        <div class="flex flex-wrap -mx-2">
+            <div class="w-full px-2 py-4">
+                <a href="/blog" class="recipe-back-button py-2 px-4 bg-blue-500 text-white text-sm rounded">Go back</a>
+                <h1 class="recipe-heading text-4xl font-bold mb-2">{{ ucfirst($recipe->recipe_name) }}</h1>
+                <p class="recipe-description text-base mb-4">{!! $recipe->recipe_description !!}</p>
                 <p><strong>Ingredients:</strong> {{ $recipe->recipe_ingredients }}</p> 
                 <p><strong>Cuisine:</strong> {{ $recipe->cuisine }}</p>
-                <hr>
-                <a href="/blog/{{ $recipe->id }}/edit" class="btn btn-outline-primary">Edit Post</a>
+                <hr class="mb-4">
+                <a href="/blog/{{ $recipe->id }}/edit" class="recipe-edit-button py-2 px-4 bg-blue-500 text-white text-sm rounded">Edit Post</a>
                 <br><br>
                 <form id="delete-frm" class="" action="" method="POST">
                     @method('DELETE')
                     @csrf
-                    <button class="btn btn-danger">Delete Post</button>
+                    <button class="recipe-delete-button py-2 px-4 bg-red-500 text-white text-sm rounded">Delete Post</button>
                 </form>
             </div>
         </div>
